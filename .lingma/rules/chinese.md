0122619bc4953a62c9ba34ede696dbad551ad13d
Jiang <3202077880@qq.com>
2026-03-21 12:23:23 +0800

    renew structure

---
+++ .lingma/rules/chinese.md
@@ -0,0 +1,5 @@
+---
+trigger: always_on
+---
+
+回答用中文
--- ultralytics/img_size.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-# Open an image
-img = Image.open('D:\Documents\code\python\yolo\datasets\defect\images\\train\\1.jpg')
-
-# Get image size
-width, height = img.size
-print(f"Image size: {width} x {height}")
